bb8f5f5963e56e21859157915917214f5f6bacb3
MaaTh4 <127776316+MaaTh4@users.noreply.github.com>
2023-12-11 00:54:32 -0300

    Add files via upload

---
+++ Wheels/src/UI/IssueBikeUI.java
@@ -0,0 +1,44 @@
+package UI;
+
+import AluguelEPagamentos.Hire;
+import AluguelEPagamentos.Payment;
+import Loja.Bike;
+import Loja.Customer;
+
+import java.util.Date;
+public class IssueBikeUI {
+
+    private Bike chosenBike = null;
+    private Customer customer = null;
+    private Payment payment = null;
+    private Hire hire = null;
+    private int numberOfDays = 0;
+
+    public void showBikeDetails(int bikeNum) {
+
+        chosenBike = Bike.findBikeByNumber(bikeNum);
+        if (chosenBike != null) {
+
+            chosenBike.showDetails();
+        }
+    }
+
+    public void calculateCost(int numDays) {
+
+        numberOfDays = numDays;
+
+        chosenBike.calculateCost(numDays);
+    }
+
+    public void createCustomer(String name, String postcode, int telephone) {
+
+        customer = new Customer(name, postcode, telephone);
+        payment = new Payment(customer);
+        hire = new Hire(new Date(), numberOfDays, chosenBike, customer);
+    }
+
+    public void calculateTotalPayment() {
+
+        payment.calculateTotalPayment(hire);
+    }
+}
